19146a1ed38daa75994dffe1de179cf7a93d91c3
Örjan Fors <o@42mm.org>
2023-01-30 22:49:07 +0100

    Fix clamshell mode in newer kernels

--- bin/sway-reload.sh
+++ bin/sway-reload.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
-if grep -q open /proc/acpi/button/lid/LID/state; then
+if grep -q open /proc/acpi/button/lid/LID0/state; then
   swaymsg output eDP-1 enable
 else
   swaymsg output eDP-1 disable
