76197d52b074d2cbc463e2a08b4fe0bbd4f74929
Örjan Fors <o@42mm.org>
2023-04-27 21:19:55 +0200

    opt.hlsearch works now

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -49,7 +49,7 @@ opt.cinoptions = ":0,g0,p2s,t0,+1s,(0"
 ---
 vim.cmd("set path+=**")
 vim.cmd("set wildignore+=*/tags,*/tmp/*,/var/*,*.a,*.o,*.obj,*.so,*.swp,*.zip,*.tar,*.pyc")
-vim.o.hlsearch = true
+opt.hlsearch = true
 
 --
 -- Completion
