1e76f6ee5917aa17ead1cd39fcba6e894775bd30
Örjan Fors <o@42mm.org>
2023-06-28 10:28:26 +0200

    Add markdown previewers

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -2,6 +2,17 @@ return {
   -- disable automatic insertion of pairs like parenthesis etc
   { "echasnovski/mini.pairs", enabled = false },
 
+  -- markdown preview, use :Glow to open preview within nvim.
+  { "ellisonleao/glow.nvim", config = true, cmd = "Glow" },
+
+  -- markdown preview, use :MarkdownPreview to preview in browser.
+  {
+    "iamcco/markdown-preview.nvim",
+    config = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
+
   {
     "hrsh7th/nvim-cmp",
     dependencies = {
