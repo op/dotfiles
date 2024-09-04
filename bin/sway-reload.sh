b0c6ab4f626f820ed45e9f93834da15c0c069191
Örjan Fors <o@42mm.org>
2024-09-04 23:44:24 +0200

    bin: new hardware

--- bin/sway-reload.sh
+++ bin/sway-reload.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-if grep -q open /proc/acpi/button/lid/LID0/state; then
-  swaymsg output eDP-1 enable
+if grep -q open /proc/acpi/button/lid/LID/state; then
+	swaymsg output eDP-1 enable
 else
-  swaymsg output eDP-1 disable
+	swaymsg output eDP-1 disable
 fi
