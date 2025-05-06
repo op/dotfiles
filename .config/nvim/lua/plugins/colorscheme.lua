e19c430ffd6c17b04adcd7e18ed93be2a2c0c1a0
Örjan Fors <o@42mm.org>
2025-05-06 08:51:05 +0200

    nvim: use latest catppuccin release

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,6 @@
 return {
   { "rose-pine/neovim", name = "rose-pine", opts = { dark_variant = "main" } },
-  { "catppuccin/nvim", name = "catppuccin", version = "0b2437" },
+  { "catppuccin/nvim", name = "catppuccin" },
   -- { "LazyVim/LazyVim", opts = { colorscheme = "rose-pine" } },
   { "LazyVim/LazyVim", opts = { colorscheme = "catppuccin" } },
 }
