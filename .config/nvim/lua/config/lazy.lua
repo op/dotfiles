1a0eeb29a8d07d6a666ff44da39d0dcf069402f5
Örjan Fors <o@42mm.org>
2026-06-23 10:04:03 +0200

    nvim: enable yanky

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -29,6 +29,7 @@ require("lazy").setup({
     -- -- { import = "lazyvim.plugins.extras.editor.dial" },
     -- -- { import = "lazyvim.plugins.extras.editor.refactoring" },
     -- -- { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.coding.yanky" },
     -- { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.git" },
