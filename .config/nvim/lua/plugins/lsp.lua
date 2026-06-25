258302e808afec90cd6d8366c0d64c0c3dc8cd77
Örjan Fors <o@42mm.org>
2026-06-25 09:46:31 +0200

    nvim: consistent comments

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -70,16 +70,16 @@ return {
             numThreads = 4,
 
             cargo = {
-              -- keep separate target directory to not block cargo
+              -- Keep separate target directory to not block cargo
               -- https://github.com/rust-lang/rust-analyzer/issues/4616
               -- https://github.com/rust-lang/rust-analyzer/issues/6007
               targetDir = true,
 
-              -- use default to not build all examples etc. Mostly to keep memory
+              -- Use default to not build all examples etc. Mostly to keep memory
               -- down. This has some big drawbacks.
               features = "default",
 
-              -- disable extra build scripts that seem to trigger OOM due to cc1 duckdb
+              -- Disable extra build scripts that seem to trigger OOM due to cc1 duckdb
               buildScripts = { enable = false },
 
               extraEnv = {
